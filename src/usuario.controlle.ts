13ccb16bf53877dd9ccfad7cac68dfdceeacf10a
eduslug <eduslug93@gmail.com>
2023-09-24 12:57:33 -0300

    list method Get list

--- src/usuario.controlle.ts
+++ src/usuario.controlle.ts
@@ -1,19 +1,40 @@
-import { Controller, Get, Header, Post, Redirect, Req, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  Post,
+  Redirect,
+  Req,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { UsuarioRepository } from './usuario.repository';
 
 @Controller('/usuarios')
 export class UsuarioController {
-    private usuarioRepository = new UsuarioRepository ()
+  private usuarioRepository = new UsuarioRepository();
   @Post()
   @Header('Cache-Control', 'teste')
   async criarUsuario(@Body() dadosDousuario: string) {
-    this.usuarioRepository.salvar(dadosDousuario)
+    this.usuarioRepository.salvar(dadosDousuario);
     return dadosDousuario;
   }
 
   @Get()
   @Header('Cache-Control', 'teste')
-  async postUsuario(dadosDousuario: string) {
+  async ListarUsuario(dadosDousuario: string) {
+    return this.usuarioRepository.listar();
+  }
+
+  @Put()
+  @Header('Cache-Control', 'teste')
+  async atualizarUsuario(@Body() dadosDousuario: string) {
+    return dadosDousuario;
+  }
+  @Delete()
+  @Header('Cache-Control', 'teste')
+  async deletarUsuario(dadosDousuario: string) {
     return dadosDousuario;
   }
 }
--- src/usuario.repository.ts
+++ src/usuario.repository.ts
@@ -1,8 +1,11 @@
 export class UsuarioRepository {
   private usuario = [];
-
+  
   async salvar(usuario) {
     this.usuario.push(usuario);
     console.log(this.usuario);
+  }
+  async listar(){
+    return this.usuario
   }
 }
